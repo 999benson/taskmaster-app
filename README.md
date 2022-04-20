c8516135acf139a9cad7efd86a5bef8a66b68bef
999benson <bensonwu509@gmail.com>
2022-04-20 23:26:01 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# TaskMaster App
+
+TaskMaster is essentially a digital to-do list for people who wish to stay on top of their tasks and keep track of their progress. The main functionality and purpose is a list so there should not be too many functionalities for users to get distracted with. Interface will be clean and straightforward to use. Users may track their progress as they use the app. The tracker will reflect user’s history of completion rate, so they know which period they have been slacking off and which periods they are staying on top of things. Users may add new lists of their tasks for the day and cross it off when it is done. The crossed-off task will be pushed to a "Done" list and the progress tracker will update the task completion progress of the day.
+Users may also track their history of tasks by querying by date or keyword. 
+
+# Business Requirements
+
+Business requirements and component diagram [here](https://docs.google.com/document/d/1v9IFRG1XjUBgadWerZoEqaBG7X_TkDjydVirW6L8_Zk/edit)
+
+
+# Usage
+
+After git clone run ```yarn install```
+
+run ```yarn start```
+
+# Deployment
+
+Project deployed on surge
+
---
+++ src/components/TaskDoneManager.js
@@ -0,0 +1,73 @@
+import minimongo from "minimongo";
+
+const IndexedDb = minimongo.IndexedDb;
+
+//default parameter "TaskDoneDB"
+function TaskDoneManager(_dbName = "TaskDoneDB") {
+  const tdm = {};
+
+  const dbName = _dbName;
+
+  /**
+   * Inserts task object that is completed into database
+   * @param {object} takes the task object
+   * @returns a promise
+   */
+  tdm.saveDoneList = (doneTask) => {
+    return new Promise((resolve, reject) => {
+      // Create IndexedDb
+      const db = new IndexedDb(
+        { namespace: dbName },
+        function () {
+          // Add a collection to the database
+          db.addCollection(
+            "tasks",
+            function () {
+              db.tasks.upsert(doneTask, resolve, reject);
+            },
+            reject
+          );
+        },
+        reject
+      );
+    });
+  };
+
+  tdm.searchDoneWord = (query) => {
+    const db = new IndexedDb(
+      { namespace: dbName },
+      function () {
+        // Add a collection to the database
+        db.addCollection("tasks", function () {
+          db.tasks.findOne({ text: { $regex: query } }, {}, function (res) {
+            console.log("TASK SEARCH: " + res.text);
+          });
+        });
+      },
+      function () {
+        alert("some error!");
+      }
+    );
+  };
+
+  tdm.searchDoneDate = (query) => {
+    console.log(typeof query);
+    const db = new IndexedDb(
+      { namespace: dbName },
+      function () {
+        // Add a collection to the database
+        db.addCollection("tasks", function () {
+          db.tasks.findOne({ date: query }, {}, function (res) {
+            console.log("DATE SEARCH: " + res.date);
+          });
+        });
+      },
+      function () {
+        alert("some error!");
+      }
+    );
+  };
+  return tdm;
+}
+
+export default TaskDoneManager;
